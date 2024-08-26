adabefcd28b82e4ed47d49b31f7c1c2171cba407
Treata11 <treata11@yahoo.com>
2024-08-26 22:48:12 +0330

    Added comments.

--- GLFW Guide/triangle-opengl.c
+++ GLFW Guide/triangle-opengl.c
@@ -1,9 +1,11 @@
-//
-//  triangle-opengl.c
-//  GLFW Guide
-//
-//  Created by Treata Norouzi on 8/24/24.
-//
+/*
+ triangle-opengl.c
+ GLFW Guide
+
+ Created by Treata Norouzi on 8/24/24.
+ 
+ Based on: https://www.glfw.org/docs/latest/quick_guide.html
+*/
 
 #include <stdio.h>
 
@@ -26,8 +28,6 @@ typedef struct Vertex
 {
     vec2 pos;
     vec3 col;
-//    vector_float2 pos;
-//    vector_float3 col;
 } Vertex;
  
 static const Vertex vertices[3] =
@@ -63,11 +63,15 @@ static void error_callback(int error, const char* description)
     fprintf(stderr, "Error: %s\n", description);
 }
  
+// Receiving input events
+/// Quit with press of `esc`
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
+
+// MARK: - Main
  
 int main(void)
 {
@@ -76,42 +80,65 @@ int main(void)
     if (!glfwInit())
         exit(EXIT_FAILURE);
  
+    // - Window Hints
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
- 
+    // Doesn't work in window-mode
+    glfwWindowHint(GLFW_REFRESH_RATE, 30);
+    
+    /**
+     Window & Context Creation
+     
+     As the window and context are inseparably linked, the window object also serves as the context handle.
+     */
     GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL Triangle", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+    
+    /// For windowed mode windows, this function returns `NULL`. This is how to tell full screen windows from windowed mode windows.
+    GLFWmonitor* monitor = glfwGetWindowMonitor(window);
+
+    if (monitor) {
+        const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+        
+        glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+    }
  
     glfwSetKeyCallback(window, key_callback);
  
+    // - Making the OpenGL context current
     glfwMakeContextCurrent(window);
     
 //    gladLoadGL(glfwGetProcAddress);
     gladLoadGL();
     
+    // setting the `vsync` latency; It's overriden by macOS settings
     glfwSwapInterval(1);
  
     // NOTE: OpenGL error checks have been omitted for brevity
  
     GLuint vertex_buffer;
     
+    // - Build Vertex-shader buffers
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
  
+    // Comiple Vertex-shader
     const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShader(vertex_shader);
  
+    // Comiple Fragment-shader
     const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
     glCompileShader(fragment_shader);
  
+    // - Create the Shaders program
     const GLuint program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
@@ -131,29 +158,40 @@ int main(void)
     glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
                           sizeof(Vertex), (void*) offsetof(Vertex, col));
  
+    // - Draw
     while (!glfwWindowShouldClose(window))
     {
         int width, height;
+        // The framebuffer size needs to be retrieved for glViewport
         glfwGetFramebufferSize(window, &width, &height);
         const float ratio = width / (float) height;
  
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
- 
+        
+        // - Reading the timer; for animation
+        double time = glfwGetTime();
+        
         mat4x4 m, p, mvp;
         mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+        mat4x4_rotate_Z(m, m, time);
         mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         mat4x4_mul(mvp, p, m);
-//        matrix_float4x4 m, p, mvp;
-//        matrix_identity_float4x4(m);
  
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
         glBindVertexArray(vertex_array);
         glDrawArrays(GL_TRIANGLES, 0, 3);
  
+        /*
+         - Swapping buffers
+         When the entire frame has been rendered, the buffers need to be swapped
+         with one another, so the back buffer becomes the front buffer and vice versa.
+         */
         glfwSwapBuffers(window);
+        
+        // - Processing events
+        // There two methods for processing pending events; polling and waiting.
         glfwPollEvents();
     }
  
